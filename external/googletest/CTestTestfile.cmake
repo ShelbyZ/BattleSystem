0c488f2defbe9e24e79dd9d95a71c2a9f930e399
ShelbyZ <user@shagman.codes>
2018-03-31 10:56:18 -0700

    Removing GTest external folder and using libgtest/vcpkg instead

--- external/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: Y:/Documents/Projects/BattleSystem/external/googletest
-# Build directory: Y:/Documents/Projects/BattleSystem/external/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
