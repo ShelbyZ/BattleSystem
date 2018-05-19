98b7b6ac28a057ae25a5a48f8d1f8fae67ba00ff
ShelbyZ <user@shagman.codes>
2018-05-19 13:01:52 -0700

    Adding entity and updating tests

--- src/arena/main.cpp
+++ src/arena/main.cpp
@@ -4,10 +4,10 @@
 
 int main(int argc, char *argv[])
 {
-    auto system = CBattleSystem();
-    std::cout << "Value is: " << system.GetValue() << "\n";
-    system.SetValue(199);
-    std::cout << "Value is: " << system.GetValue() << "\n";
+    // auto system = CBattleSystem();
+    // std::cout << "Value is: " << system.GetValue() << "\n";
+    // system.SetValue(199);
+    // std::cout << "Value is: " << system.GetValue() << "\n";
 
 	return 0;
 }
--- src/battle_system/battle_system.h
+++ src/battle_system/battle_system.h
@@ -1,17 +1,29 @@
 #ifndef _BATTLE_SYSTEM_H
 #define _BATTLE_SYSTEM_H
 
+#include <unordered_map>
+#include <vector>
+
 #include "battle_system_export.h"
+
+#include "entity.h"
 
 class BATTLE_SYSTEM_EXPORT CBattleSystem
 {
     public:
+        enum class EntityType
+        {
+            UNKNOWN,
+            PLAYER,
+            ENEMY
+        };
         CBattleSystem(void);
-        int GetValue() const;
-        void SetValue(int value);
+        void registerEntity(CEntity&, EntityType);
+        const std::vector<CEntity>& getEntities(EntityType) const;
 
     private:
-        int _value;
+        bool isValidType(EntityType) const;
+        std::unordered_map<EntityType, std::vector<CEntity>> _entities;
 };
 
 #endif
